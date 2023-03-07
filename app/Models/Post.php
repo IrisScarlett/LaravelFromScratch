d3c31e47af831f6f9ac91bb79edd6cf2b9fd2460
Iris Scarlett <82521325+IrisScarlett@users.noreply.github.com>
2023-03-07 14:04:00 -0300

    blade structures

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post {
 
@@ -26,18 +27,34 @@ class Post {
     }
 
     public static function all(){
-        $files = File::files(resource_path("posts/"));
-
-        return array_map(fn($file) => $file->getContents(), $files);
+        return cache()->rememberForever('posts.all', function(){
+            return collect(File::files(resource_path("posts")))
+            ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn ($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+            ->sortBy('date');
+        });
     }
 
     public static function find($slug){
 
-        if (! file_exists($path = resource_path("/posts/{$slug}.html"))){
+        return static::all()->firstWhere('slug', $slug);
+
+    }
+
+    public static function findOrFail($slug){
+        $post = static::find($slug);
+
+        if (! $post) {
             throw new ModelNotFoundException();
         }
 
-        return cache()->remember("post.{$slug}", now()->addMinutes(20), fn() => file_get_contents($path));
+        return $post;
 
     }
 
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HolaController;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,34 +16,16 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-    $files = File::files(resource_path("posts/"));
-    $posts = [];
-
-    foreach($files as $file){
-        $document = YamlFrontMatter::parseFile($file);
-
-        $posts[] = new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body()
-        );
-    }
-
-
-    ddd($posts);
-
-
     return view('posts', [
-        'posts' => $posts
+        'posts' => Post::all()
     ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
     return view('post', [
-        'post' => Post::find($slug)
+        'post' => Post::findOrFail($slug)
     ]);
 
-})->where('post', '[A-z_\-]+');
+});
 
 Route::get('/hola/{name}', [HolaController::class, "index"]);
